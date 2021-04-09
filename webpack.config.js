896ca9cbdd2d10a777234466080ede038352fc3f
Gabriel Alejandro Gomez <gabriel70@gmail.com>
2021-04-09 13:17:08 -0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+const path = require('path')
+const htmWebpackPlugin = require('html-webpack-plugin')
+const miniCssExtractPlugin = require('mini-css-extract-plugin')
+const copyWebpackPlugin = require('copy-webpack-plugin')
+const cssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const terserPlugin = require('terser-webpack-plugin')
+const dotEnv = require('dotenv-webpack')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    assetModuleFilename: "assets/images/[hash][ext][query]"
+  },
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@utils': path.resolve(__dirname, 'src/utils/'),
+      '@templates': path.resolve(__dirname, 'src/templates/'),
+      '@styles': path.resolve(__dirname, 'src/styles/'),
+      '@images': path.resolve(__dirname, 'src/assets/images/'),
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          miniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.png/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            nimetype: "aplicaion/font-woff",
+            name: "[name].[contenthash].[ext]",
+            outputPath: "./assets/fonts/",
+            publicPath: "../assets/fonts/",
+            esModule: false,
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new htmWebpackPlugin({
+      inject: true,
+      template: './public/index.html',
+      finename: './index.html'
+    }),
+    new miniCssExtractPlugin({
+      filename: 'assets/[name][contenthash].css'
+    }),
+    new copyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "assets/images"),
+          to: "assets/images",
+        }
+      ]
+    }),
+    new dotEnv(),
+    new CleanWebpackPlugin(),
+  ] ,
+  optimization: {
+    minimize: true,
+    minimizer: [
+     new cssMinimizerPlugin(),
+     new terserPlugin
+    ]
+  }
+  }
